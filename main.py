e80c1848ef913a73156bbeb966272f9f4fdd46eb
tobkirch <159419620+tobkirch@users.noreply.github.com>
2024-02-09 09:48:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,27 @@
 import streamlit as st
-import numpy as np
-from streamlit_cropper import st_cropper
 from PIL import Image
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+def main():
+    st.title("Bildzuschnitts-App")
+    st.write("Lade ein Bild hoch und wähle den Bereich aus, den du zuschneiden möchtest.")
 
-# Upload an image and set some options for demo purposes
-st.header("Cropper Demo")
-img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'])
-realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
-box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
-stroke_width = st.sidebar.number_input(label="Box Thickness", value=3, step=1)
+    # Bild hochladen
+    uploaded_image = st.file_uploader("Bild auswählen", type=["jpg", "jpeg", "png"])
 
-aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
-aspect_dict = {
-    "1:1": (1, 1),
-    "16:9": (16, 9),
-    "4:3": (4, 3),
-    "2:3": (2, 3),
-    "Free": None
-}
-aspect_ratio = aspect_dict[aspect_choice]
+    if uploaded_image is not None:
+        # Bild anzeigen
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Originalbild", use_column_width=True)
 
-return_type_choice = st.sidebar.radio(label="Return type", options=["Cropped image", "Rect coords"])
-return_type_dict = {
-    "Cropped image": "image",
-    "Rect coords": "box"
-}
-return_type = return_type_dict[return_type_choice]
+        # Zuschnittbereich auswählen
+        st.write("Wähle den Bereich aus, den du zuschneiden möchtest.")
+        left, top, right, bottom = st.slider("Wähle den linken, oberen, rechten und unteren Rand:", 0, image.width, (0, 0, image.width, image.height))
 
-if img_file:
-    img = Image.open(img_file)
-    if not realtime_update:
-        st.write("Double click to save crop")
-    if return_type == 'box':
-        rect = st_cropper(
-            img,
-            realtime_update=realtime_update,
-            box_color=box_color,
-            aspect_ratio=aspect_ratio,
-            return_type=return_type,
-            stroke_width=stroke_width
-        )
-        raw_image = np.asarray(img).astype('uint8')
-        left, top, width, height = tuple(map(int, rect.values()))
-        st.write(rect)
-        masked_image = np.zeros(raw_image.shape, dtype='uint8')
-        masked_image[top:top + height, left:left + width] = raw_image[top:top + height, left:left + width]
-        st.image(Image.fromarray(masked_image), caption='masked image')
-    else:
-        # Get a cropped image from the frontend
-        cropped_img = st_cropper(
-            img,
-            realtime_update=realtime_update,
-            box_color=box_color,
-            aspect_ratio=aspect_ratio,
-            return_type=return_type,
-            stroke_width=stroke_width
-        )
+        # Bild zuschneiden
+        cropped_image = image.crop((left, top, right, bottom))
 
-        # Manipulate cropped image at will
-        st.write("Preview")
-        _ = cropped_img.thumbnail((150, 150))
-        st.image(cropped_img)
+        # Zugeschnittenes Bild anzeigen
+        st.image(cropped_image, caption="Zugeschnittenes Bild", use_column_width=True)
+
+if __name__ == "__main__":
+    main()
